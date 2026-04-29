cca75386c9ff8018799616c6a9e8d940b76b587c
mrfufl4ik <mrfufl4ik@gmail.com>
2026-04-30 01:37:53 +0600

    - Mini-changes

--- src/keyboards/connect_keyboard.py
+++ src/keyboards/connect_keyboard.py
@@ -1,9 +1,11 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CopyTextButton
 
 from src.core.telegram_bot import create_cancel_button
+from src.dynamic.config import localisation_config
 
+__connect_menu_localisation: dict = localisation_config["connect_menu"]
 
-async def get_keyboard() -> InlineKeyboardMarkup:
+async def get_keyboard(connect_link: str) -> InlineKeyboardMarkup:
     result = []
 
     cancel_button = create_cancel_button("start_menu")
--- src/routers/connect_router.py
+++ src/routers/connect_router.py
@@ -5,7 +5,7 @@ from aiogram.types import CallbackQuery, InputMediaPhoto
 from src.core.telegram_bot import get_cached_image
 from src.data.bot_user import get_bot_user_by_telegram_id
 from src.data.product_user import get_product_user, is_product_user_active
-from src.dynamic.config import localisation_config
+from src.dynamic.config import localisation_config, main_config
 from src.dynamic.connect_image import get_connect_image
 from src.keyboards.connect_keyboard import get_keyboard
 from src.routers.start_router import init_start_message
@@ -13,6 +13,7 @@ from src.routers.start_router import init_start_message
 router = Router()
 
 __connect_menu_localisation: dict = localisation_config["connect_menu"]
+__connect_config: dict = main_config["connect"]
 
 @router.callback_query(lambda c: c.data == "connect_menu")
 async def __on_callback(callback: CallbackQuery):
@@ -26,8 +27,13 @@ async def __on_callback(callback: CallbackQuery):
     connect_image = await get_connect_image(product_user)
     media = await get_cached_image(connect_image)
     caption = __connect_menu_localisation.get("caption", "{0}")
-    caption = caption.format(product_user.connect_link)
-    keyboard = await get_keyboard()
+    caption = caption.format(
+        product_user.connect_link,
+        __connect_config.get("download_link_windows", "https://google.com"),
+        __connect_config.get("download_link_linux", "https://google.com"),
+        __connect_config.get("download_link_android", "https://google.com"),
+    )
+    keyboard = await get_keyboard(product_user.connect_link)
     await callback.message.edit_media(
         InputMediaPhoto(
             media=media,
