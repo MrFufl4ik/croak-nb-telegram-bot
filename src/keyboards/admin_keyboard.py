1bfb86c2f6d654c2de7ad1e3744d6e2bee489068
mrfufl4ik <mrfufl4ik@gmail.com>
2026-05-02 17:37:09 +0600

    - Userinfo system

--- src/keyboards/admin_keyboard.py
+++ src/keyboards/admin_keyboard.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatFullInfo
 
 from src.core.telegram_bot import create_cancel_button
 from src.data.bot_user import BotUser
@@ -9,6 +9,19 @@ async def get_keyboard(bot_user: BotUser) -> InlineKeyboardMarkup:
 
     cancel_button = create_cancel_button("start_menu")
     result.append([cancel_button])
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=result)
+    return keyboard
+
+async def get_userinfo_keyboard(chat_info: ChatFullInfo) -> InlineKeyboardMarkup:
+    result = []
+
+    go_to_account_button = InlineKeyboardButton(
+        text="go to account",
+        url=f"tg://user?id={chat_info.id}",
+        style="success"
+    )
+    result.append([go_to_account_button])
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=result)
     return keyboard
--- src/routers/admin_router.py
+++ src/routers/admin_router.py
@@ -1,12 +1,15 @@
+import re
+
 from aiogram import Router
 from aiogram.enums import ParseMode
-from aiogram.types import CallbackQuery, InputMediaPhoto
+from aiogram.filters import Command
+from aiogram.types import CallbackQuery, InputMediaPhoto, Message
 
-from src.core.telegram_bot import get_cached_image
+from src.core.telegram_bot import get_cached_image, get_telegram_bot
 from src.data.bot_user import get_bot_user_by_telegram_id
 from src.dynamic import image
 from src.dynamic.config import localisation_config
-from src.keyboards.admin_keyboard import get_keyboard
+from src.keyboards.admin_keyboard import get_keyboard, get_userinfo_keyboard
 
 router = Router()
 
@@ -28,4 +31,36 @@ async def __on_callback(callback: CallbackQuery):
             parse_mode=ParseMode.HTML
         ),
         reply_markup=keyboard
+    )
+
+
+@router.message(Command("userinfo"))
+async def __on_callback(message: Message):
+    bot_user = await get_bot_user_by_telegram_id(message.from_user.id)
+    if not bot_user.is_admin: return
+    regex_pattern = r"^/userinfo (\d+)$"
+    if not re.fullmatch(regex_pattern, message.text): return
+    match = re.match(regex_pattern, message.text)
+    user_id = match.group(1)
+    telegram_bot = get_telegram_bot()
+    chat_info = await telegram_bot.get_chat(user_id)
+    result = []
+    result.append("========================")
+    result.append("<u>id</u> | {0}".format(chat_info.id))
+    result.append("<u>username</u> | {0}".format(chat_info.username))
+    result.append("<u>full name</u> | {0}".format(chat_info.full_name))
+    result.append("<u>bio</u> | {0}".format(chat_info.bio))
+    if chat_info.birthdate is not None:
+        birthdate_caption = "{}.{}.{}".format(
+            chat_info.birthdate.day,
+            chat_info.birthdate.month,
+            chat_info.birthdate.year
+        )
+        result.append("<u>birthdate</u> | {0}".format(birthdate_caption))
+
+    keyboard = await get_userinfo_keyboard(chat_info)
+    await message.answer(
+        text='\n'.join(result),
+        reply_markup=keyboard,
+        parse_mode=ParseMode.HTML
     )
