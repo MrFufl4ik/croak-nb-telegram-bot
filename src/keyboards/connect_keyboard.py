6f81c3c79fd28329f860b002bf9e2f5ddf1eae6d
mrfufl4ik <mrfufl4ik@gmail.com>
2026-07-23 13:49:00 +0600

    - Remove wl connect

--- src/keyboards/connect_keyboard.py
+++ src/keyboards/connect_keyboard.py
@@ -15,14 +15,14 @@ async def get_keyboard() -> InlineKeyboardMarkup:
         callback_data="main_connect_menu",
         style="primary"
     )
-    wl_connect_menu_button = InlineKeyboardButton(
-        text=__connect_menu_localisation.get("wl_connect_menu_button", "Connect [WL]"),
-        callback_data="wl_connect_menu",
-        style="primary"
-    )
+    # wl_connect_menu_button = InlineKeyboardButton(
+    #     text=__connect_menu_localisation.get("wl_connect_menu_button", "Connect [WL]"),
+    #     callback_data="wl_connect_menu",
+    #     style="primary"
+    # )
 
     result.append([main_connect_menu_button])
-    result.append([wl_connect_menu_button])
+    # result.append([wl_connect_menu_button])
     result.append([cancel_button])
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=result)
